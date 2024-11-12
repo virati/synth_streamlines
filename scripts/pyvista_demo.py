c118751811a5bc642e6859cf967d8b76369d63b6
Vineet Tiruvadi <virati@gmail.com>
2024-11-12 12:27:02 -0800

    initial test to coincide points and splines

--- scripts/pyvista_demo.py
+++ scripts/pyvista_demo.py
@@ -1,6 +1,21 @@
 import numpy as np
 import pyvista
 
+
+#%% Point Cloud
+
+rng = np.random.default_rng(seed=0)
+point_cloud = rng.random((100, 3))
+pdata = pyvista.PolyData(point_cloud)
+pdata['orig_sphere'] = np.arange(100)
+
+# create many spheres from the point cloud
+sphere = pyvista.Sphere(radius=0.02, phi_resolution=10, theta_resolution=10)
+pc = pdata.glyph(scale=False, geom=sphere, orient=False)
+#pc.plot(cmap='Reds')
+
+#%% Splines
+
 # Make the xyz points
 theta = np.linspace(-10 * np.pi, 10 * np.pi, 100)
 z = np.linspace(-2, 2, 100)
