2616a3bbf6cf4b010b2306274a16bda3bc7e585f
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2024-11-12 12:13:11 -0800

    script plots

--- scripts/pyvista_demo.py
+++ scripts/pyvista_demo.py
@@ -1,3 +1,4 @@
+#%%
 from __future__ import annotations
 
 import time
@@ -11,3 +12,6 @@ tstart = time.time()
 clipped = lidar.clip(origin=(0, 0, 1.76e3), normal=(0, 0, 1))
 t_elapsed = time.time() - tstart
 print(f"Time to clip with a PolyData {t_elapsed:.2f} seconds.")
+
+#%%
+clipped.plot(show_scalar_bar=False)
