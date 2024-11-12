00577b95be243d21f37bcd5451735791c9bc2f43
Vineet Tiruvadi <virati@gmail.com>
2024-11-12 12:25:28 -0800

    spline tutorial

--- scripts/pyvista_demo.py
+++ scripts/pyvista_demo.py
@@ -1,17 +1,13 @@
-#%%
-from __future__ import annotations
+import numpy as np
+import pyvista
 
-import time
+# Make the xyz points
+theta = np.linspace(-10 * np.pi, 10 * np.pi, 100)
+z = np.linspace(-2, 2, 100)
+r = z**2 + 1
+x = r * np.sin(theta)
+y = r * np.cos(theta)
+points = np.column_stack((x, y, z))
 
-import pyvista as pv
-from pyvista import examples
-
-lidar = examples.download_lidar()
-
-tstart = time.time()
-clipped = lidar.clip(origin=(0, 0, 1.76e3), normal=(0, 0, 1))
-t_elapsed = time.time() - tstart
-print(f"Time to clip with a PolyData {t_elapsed:.2f} seconds.")
-
-#%%
-clipped.plot(show_scalar_bar=False)
+spline = pyvista.Spline(points, 500).tube(radius=0.1)
+spline.plot(scalars='arc_length', show_scalar_bar=False)
