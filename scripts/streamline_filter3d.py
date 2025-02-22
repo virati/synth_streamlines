9326083ca933ed19f18e766d9ee7df69bac72c49
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-02-21 18:27:08 -0800

    3d plotting working

--- scripts/streamline_filter3d.py
+++ scripts/streamline_filter3d.py
@@ -1,14 +1,14 @@
 # %%
-%load_ext autoreload
-%autoreload 2
+# %load_ext autoreload
+# %autoreload 2
 
 from synth_streamlines.streams.d3 import generate as gen
 import matplotlib.pyplot as plt
 
-probe_point = (0.5, 0.5,0.5,2)
+probe_point = {"x": 0.5, "y": 0.5, "z": 0.5, "r": 2}
 test = gen.bundle()
-test.generate_bundle(do_plot=True, tortuosity=0.01, noise_level=10)
-test.plot_streamlines_with_probe(test.streamlines, *probe_point)
+test.generate_bundle(do_plot=False, tortuosity=0.01, noise_level=10)
+test.plot_streamlines_with_probe(test.streamlines, **probe_point)
 # %%
 ff_task = gen.StreamlineChecker()
-ff_task.check_streamlines(test, *probe_point)
+print(ff_task.check_streamlines(test, **probe_point))
