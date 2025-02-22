80b4367ad8c53157c70a5e52e74429271c13c892
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-02-21 17:56:48 -0800

    initial streamline 2d ff synth working

---
+++ scripts/streamline_filter.py
@@ -0,0 +1,14 @@
+# %%
+%load_ext autoreload
+%autoreload 2
+
+from synth_streamlines.streams.d2 import generate as gen
+import matplotlib.pyplot as plt
+
+probe_point = (0.5, 0.5,0.1)
+test = gen.bundle()
+test.generate_bundle(do_plot=True)
+test.plot_streamlines_with_probe(test.streamlines, *probe_point)
+# %%
+ff_task = gen.StreamlineChecker()
+ff_task.check_streamlines(test, *probe_point)
