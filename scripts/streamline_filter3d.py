0075b29187adc7e6a82b6bcd8b1e23aac07f0cef
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-02-21 18:23:54 -0800

    radius changable

--- scripts/streamline_filter3d.py
+++ scripts/streamline_filter3d.py
@@ -5,7 +5,7 @@
 from synth_streamlines.streams.d3 import generate as gen
 import matplotlib.pyplot as plt
 
-probe_point = (0.5, 0.5,0.5,10)
+probe_point = (0.5, 0.5,0.5,2)
 test = gen.bundle()
 test.generate_bundle(do_plot=True, tortuosity=0.01, noise_level=10)
 test.plot_streamlines_with_probe(test.streamlines, *probe_point)
