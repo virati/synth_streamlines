c8cb35477e37fc12ff489ab27c9e0ad489e6a4c2
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-02-24 19:08:38 -0800

    working at large-scale

--- scripts/streamline_filter3d.py
+++ scripts/streamline_filter3d.py
@@ -5,10 +5,18 @@
 from synth_streamlines.streams.d3 import generate as gen
 import matplotlib.pyplot as plt
 
-probe_point = {"x": 0.5, "y": 0.5, "z": 0.5, "r": 2}
 test = gen.bundle()
-test.generate_bundle(do_plot=False, tortuosity=0.01, noise_level=10)
-test.plot_streamlines_with_probe(test.streamlines, **probe_point)
+test.generate_bundle(
+    a=(-10, -10, -10),
+    b=(10, 10, 10),
+    do_plot=False,
+    tortuosity=0.01,
+    noise_level=3.0,
+    num_streamlines=20,
+)
 # %%
+probe_point = {"x": 0.0, "y": 0.0, "z": 0.0, "r": 2}
+test.plot_streamlines_with_probe(test.streamlines, **probe_point)
 ff_task = gen.StreamlineChecker()
 print(ff_task.check_streamlines(test, **probe_point))
+print("Done")
