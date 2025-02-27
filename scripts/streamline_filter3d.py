bb823c4284f5931ee8af40f4da4f93ea4a3fd7e5
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-02-27 07:09:26 -0800

    parameter change

--- scripts/streamline_filter3d.py
+++ scripts/streamline_filter3d.py
@@ -8,14 +8,14 @@ import matplotlib.pyplot as plt
 test = gen.bundle()
 test.generate_bundle(
     a=(-10, -10, -10),
-    b=(10, 10, 10),
+    b=(10, -10, 10),
     do_plot=False,
     tortuosity=0.01,
-    noise_level=3.0,
+    noise_level=10.0,
     num_streamlines=20,
 )
 # %%
-probe_point = {"x": 0.0, "y": 0.0, "z": 0.0, "r": 2}
+probe_point = {"x": 0.0, "y": 0.0, "z": 0.0, "r": 6}
 test.plot_streamlines_with_probe(test.streamlines, **probe_point)
 ff_task = gen.StreamlineChecker()
 print(ff_task.check_streamlines(test, **probe_point))
